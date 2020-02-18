16f9a127b29abb0b7684fa050f7085db21395380
Vernet_t <cogeok12@gmail.com>
2020-02-18 15:37:39 +0900

    Updates

---
+++ precache-manifest.99750be7a7a313918d19ed5c8751a0dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c8f617ee4a4183a1903aeb4f1ba124a",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "703b6a40de1a95375887",
+    "url": "/movie_app_2020/static/css/main.c848c292.chunk.css"
+  },
+  {
+    "revision": "39b8bcc7d2d72670fac8",
+    "url": "/movie_app_2020/static/js/2.39be1832.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/movie_app_2020/static/js/2.39be1832.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "703b6a40de1a95375887",
+    "url": "/movie_app_2020/static/js/main.23cf5211.chunk.js"
+  },
+  {
+    "revision": "01aae0007e1cfd873da8",
+    "url": "/movie_app_2020/static/js/runtime-main.482f7179.js"
+  }
+]);
